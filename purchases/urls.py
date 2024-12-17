8af31e5626378db7443da1267f7747c90955bfd3
BigBossSpektrum <silvekerhernandez@proton.me>
2024-12-17 12:52:32 -0500

    update

--- purchases/urls.py
+++ purchases/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.cart_detail, name='cart_detail'),
+    # path('', views.cart_detail, name='cart_detail'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('products/', views.available_products, name='available_products'),
     path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-
-
 ]
